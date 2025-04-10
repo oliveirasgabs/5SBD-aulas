3a2fb7fe1e1a2b0cb84ab6d5ef971a403b08cc61
Gabrielly Oliveira <oliveiragabrielly.dev@gmail.com>
2025-04-09 23:23:26 -0300

    adicionando a parte 2

--- secoes-4-a-6.sql
+++ secoes-4-a-6.sql
@@ -29,3 +29,32 @@ SELECT SYSDATE + 30 AS "Data de vencimento simulada" FROM dual;
 
 -- 10.Exiba o número de dias entre a data de abertura da conta e a data atual.
 SELECT SYSDATE - data_abertura AS dias_entre_datas FROM contas;
+
+--PARTE 2
+-- 11.Apresente o saldo de cada conta formatado como moeda local.
+SELECT TO_CHAR(saldo,'R$99,999') AS saldo_formatado FROM contas
+
+-- 12. Converta a data de abertura da conta para o formato 'dd/mm/yyyy'.
+SELECT TO_CHAR(data_abertura, 'DD/MM/YYYY') AS data_formatada FROM contas;
+
+-- 13.Exiba o saldo da conta e substitua valores nulos por 0.
+SELECT NVL(saldo, 0) AS saldo_ajustado FROM contas;
+
+-- 14.Exiba os nomes dos clientes e substitua valores nulos na cidade por 'Sem cidade'.
+SELECT nome, NVL(cidade, 'Sem cidade') AS cidade_ajustada FROM contas;
+
+-- 15. Classifique os clientes em grupos com base em sua cidade:
+  -- o 'Região Metropolitana' se forem de Niterói
+  -- o 'Interior' se forem de Resende
+  --o 'Outra Região' para as demais cidades
+
+  SELECT nome, cidade,
+  CASE 
+    WHEN cidade = 'Niterói' THEN 'Região Metropolitana'
+    WHEN cidade = 'Resende' THEN 'Interior'
+    ELSE 'Outra Região'
+  END AS classificacao_regiao
+FROM contas;
+
+--PARTE 3
+
